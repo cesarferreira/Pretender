ba923f59174d995157598f4369ea23daa639b648
César Ferreira <cesar.manuel.ferreira@gmail.com>
2015-07-08 01:10:29 +0100

    first commit

---
+++ pretender/src/main/java/github/cesarferreira/pretender/Nationality.java
@@ -0,0 +1,6 @@
+package github.cesarferreira.pretender;
+
+
+public enum Nationality {
+    AU, ES, FI, FR, GB, IE, NL, US
+}
---
+++ pretender/src/main/java/github/cesarferreira/pretender/Pretender.java
@@ -0,0 +1,7 @@
+package github.cesarferreira.pretender;
+
+/**
+ * Created by cesarferreira on 08/07/15.
+ */
+public class Pretender {
+}
